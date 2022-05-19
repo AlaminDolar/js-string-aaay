5edb082e1da89f224495b5fc5290af46b4735c2c
AlaminDolar <amin15-9898@diu.edu.bd>
2022-05-19 13:17:40 +0600

    string-slice

---
+++ string-slice.js
@@ -0,0 +1,12 @@
+const anthem='Amar Sonar Bangla Ami Tomay Valobashi';
+const words=anthem.split(' ');
+// console.log(words);
+// slice
+const smallSlice= anthem.slice(5,18);
+// console.log(smallSlice);
+
+// concat
+const firt ='Alamin';
+const second ='Dolar';
+const fullname= firt.concat(second).concat('Engineer');
+console.log(fullname);
